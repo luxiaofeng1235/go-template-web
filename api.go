0c0575fb455811d30418fc37210701ca2ae5dd80
卢晓峰 <luxiaofeng.200@163.com>
2025-09-09 17:13:44 +0800

    1

--- api.go
+++ api.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	"go-web-template/db"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 )
 
+// 测试
 // API服务器启动入口 - 参考go-novel的架构设计
 func main() {
 	db.StartAPIServer()
