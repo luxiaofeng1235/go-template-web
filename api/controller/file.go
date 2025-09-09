72aa610430de6848e6a01ef59225ae844d906f63
卢晓峰 <luxiaofeng.200@163.com>
2025-09-09 20:04:19 +0800

    1

--- api/controller/file.go
+++ api/controller/file.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"go-web-template/internal/service"
+	"go-web-template/internal/service/common"
 	"go-web-template/utils"
 
 	"github.com/gogf/gf/v2/net/ghttp"
@@ -12,7 +12,7 @@ type FileController struct{}
 // FormImage 表单方式上传图片
 func (c *FileController) FormImage(r *ghttp.Request) {
 	// 直接调用Service层处理图片上传，传入默认参数
-	result, err := service.UploadImageSimple(r, 0, 0, 0)
+	result, err := common.UploadImageSimple(r, 0, 0, 0)
 	if err != nil {
 		utils.Fail(r, err, "上传失败")
 		return
@@ -24,7 +24,7 @@ func (c *FileController) FormImage(r *ghttp.Request) {
 // FormVideo 表单方式上传视频
 func (c *FileController) FormVideo(r *ghttp.Request) {
 	// 直接调用Service层处理视频上传，传入默认参数
-	result, err := service.UploadVideoSimple(r, 0, 0, 0)
+	result, err := common.UploadVideoSimple(r, 0, 0, 0)
 	if err != nil {
 		utils.Fail(r, err, "上传失败")
 		return
